35f2b4fbc601dfdcfc7c3f6e7b3ede50539a3299
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2025-01-19 18:34:16 +0100

    feat: export website as pdf link ver Next.js logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        <Image
-          alt="Next.js logo"
-          className="dark:invert"
-          height={38}
-          src="/next.svg"
-          width={180}
-          priority
-        />
+        <Link
+          className="cursor-pointer"
+          href="/api/export"
+        >
+          <Image
+            alt="Next.js logo"
+            className="dark:invert"
+            height={38}
+            src="/next.svg"
+            width={180}
+            priority
+          />
+        </Link>
         <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left">
           <li className="mb-2">
             Get started by editing{' '}
