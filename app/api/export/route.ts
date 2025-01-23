bce8928b9ba6be192d03a656b0dd281a621b8d28
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2025-01-23 10:51:10 +0100

    feat(pdf): enhance PDF generation with dark mode support and dynamic filename

--- app/api/export/route.ts
+++ app/api/export/route.ts
@@ -1,8 +1,6 @@
 import { NextRequest } from 'next/server';
 import puppeteer from 'puppeteer';
 
-const resumeFileName = 'attila-csanyi-resume.pdf';
-
 export const GET = async (request: NextRequest) => {
   try {
     const host = request.headers.get('host');
@@ -12,29 +10,60 @@ export const GET = async (request: NextRequest) => {
         : 'http';
     const baseUrl = `${protocol}://${host}`;
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     const page = await browser.newPage();
 
+    // Force dark mode
+    await page.emulateMediaFeatures([
+      { name: 'prefers-color-scheme', value: 'dark' },
+    ]);
+
+    await page.setViewport({
+      width: 794,
+      height: 1123,
+      deviceScaleFactor: 2,
+    });
+
     // Navigate to the page
-    await page.goto(`${baseUrl}`);
+    await page.goto(`${baseUrl}`, {
+      waitUntil: ['load', 'networkidle0'],
+      timeout: 60000,
+    });
 
     // Generate PDF
     const pdfBuffer = await page.pdf({
       format: 'A4',
       printBackground: true,
+      scale: 0.75, // Adjust scale to prevent content overflow
     });
 
     await browser.close();
 
+    const date = new Date()
+      .toLocaleDateString('en-GB', {
+        year: '2-digit',
+        month: '2-digit',
+        day: '2-digit',
+      })
+      .split('/')
+      .join('');
+
+    const fileName = ['attila_csanyi', 'CV', date].join('_');
+
     return new Response(pdfBuffer, {
       headers: {
         'Content-Type': 'application/pdf',
-        'Content-Disposition': `attachment; filename="${resumeFileName}"`,
+        'Content-Disposition': `attachment; filename="${fileName}.pdf"`,
       },
       status: 200,
     });
   } catch (error) {
     console.error('Error generating PDF:', error);
-    return Response.json({ error: 'Failed to generate PDF' }, { status: 500 });
+    return Response.json(
+      { error: `Failed to generate PDF: ${(error as Error).message}` },
+      { status: 500 }
+    );
   }
 };
