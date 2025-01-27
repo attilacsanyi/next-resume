f193f38444d1983777d40f8dd5531cdcd6899fd4
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2025-01-27 10:08:40 +0100

    chore(home): remove debug console log from home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ export default async function Home() {
   const {
     profile: { name, location },
   } = await getResume();
-  console.log(name, location);
 
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
