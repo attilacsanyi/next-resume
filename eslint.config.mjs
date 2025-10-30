d9d5dcd8ac79641f1693f42f38097122fecc9778
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2025-10-30 11:52:45 +0100

    chore(upgrade): Next.js 16 upgrade via codemod

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,14 @@
-import { FlatCompat } from '@eslint/eslintrc';
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import nextTypescript from 'eslint-config-next/typescript';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...nextCoreWebVitals,
+  ...nextTypescript,
   {
     rules: {
       // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-sort-props.md
@@ -35,6 +33,16 @@ const eslintConfig = [
         },
       ],
     },
+  },
+  {
+    ignores: [
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+      '.history/**',
+    ],
   },
 ];
 
