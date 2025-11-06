578116ef7c586188971c4379f99ad8eacbb66450
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2025-11-06 11:35:44 +0100

    feat(apollo): enhance Apollo Client configuration for ISR and caching

--- features/contentful/apollo.client.ts
+++ features/contentful/apollo.client.ts
@@ -1,20 +1,29 @@
 import { env } from '@/env';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const isDev = env.NODE_ENV !== 'production';
 
 export const client = new ApolloClient({
-  uri: env.CONTENTFUL_GRAPHQL_URL,
+  ssrMode: true,
+  link: createHttpLink({
+    uri: env.CONTENTFUL_GRAPHQL_URL,
+    /**
+     * Custom fetch to enable ISR with Next.js route cache (5m) and cache tags.
+     */
+    fetch: (uri, options) => {
+      return fetch(uri, {
+        ...options,
+        next: { revalidate: 300, tags: ['contentful', 'resume'] },
+      });
+    },
+  }),
   cache: new InMemoryCache(),
   defaultOptions: {
     query: {
       /**
-       * The default is 'cache-first'.
+       * Dev: never cache; Prod: let Next.js route cache (5m) + Apollo cache-first.
        * Docs: https://www.apollographql.com/docs/react/data/queries/#cache-first
-       *
-       * Use 'no-cache' for server-side requests to ensure fresh data
-       * on each page refresh. This prevents Apollo from caching responses.
-       * */
+       */
       fetchPolicy: isDev ? 'no-cache' : 'cache-first',
     },
   },
