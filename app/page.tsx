826ef50ec6b0987126659241618bdc7dd730d43d
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2025-11-06 11:51:06 +0100

    feat(print): enhance print styles for better formatting and visibility

--- app/page.tsx
+++ app/page.tsx
@@ -1,110 +1,31 @@
-import TailwindDemo from '@/components/tailwind-demo';
 import { env } from '@/env';
-import { getResume } from '@/features/contentful/contentful.utils';
-import Image from 'next/image';
+import {
+  DevelopmentsSection,
+  EducationsSection,
+  ExperiencesSection,
+  LearningsSection,
+  ProfileSection,
+  getResume,
+} from '@/features/resume';
 
-export default async function Home() {
+export const revalidate = 300;
+
+const Home = async () => {
   const isDev = env.NODE_ENV !== 'production';
-  const {
-    profile: { name, location },
-  } = await getResume(isDev);
+  const { profile, experiences, learnings, developments, educations } =
+    await getResume(isDev);
 
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-(family-name:--font-geist-sans) sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        <Image
-          alt="Next.js logo"
-          className="dark:invert"
-          height={38}
-          src="/next.svg"
-          width={180}
-          priority
-        />
-        <ol className="list-inside list-decimal text-center font-(family-name:--font-geist-mono) text-sm sm:text-left">
-          <li className="mb-2">
-            {name}
-            <code className="rounded-sm bg-black/5 px-1 py-0.5 font-semibold dark:bg-white/6">
-              {location.city}
-            </code>
-          </li>
-          <li>{location.country}</li>
-        </ol>
-
-        <TailwindDemo />
-
-        <div className="flex flex-col items-center gap-4 sm:flex-row">
-          <a
-            className="bg-foreground text-background flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent px-4 text-sm transition-colors hover:bg-[#383838] sm:h-12 sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <Image
-              alt="Vercel logomark"
-              className="dark:invert"
-              height={20}
-              src="/vercel.svg"
-              width={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="flex h-10 items-center justify-center rounded-full border border-solid border-black/8 px-4 text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <Image
-            alt="File icon"
-            height={16}
-            src="/file.svg"
-            width={16}
-            aria-hidden
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <Image
-            alt="Window icon"
-            height={16}
-            src="/window.svg"
-            width={16}
-            aria-hidden
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <Image
-            alt="Globe icon"
-            height={16}
-            src="/globe.svg"
-            width={16}
-            aria-hidden
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main className="min-h-screen">
+      <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+        <ProfileSection profile={profile} />
+        <ExperiencesSection experiences={experiences} />
+        <LearningsSection learnings={learnings} />
+        <DevelopmentsSection developments={developments} />
+        <EducationsSection educations={educations} />
+      </div>
+    </main>
   );
-}
+};
+
+export default Home;
