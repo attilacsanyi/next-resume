a75e72ea2e860d27d2a09fbe1b777af43bb11665
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2025-11-06 17:02:09 +0100

    refactor(layout): replace font with Poppins and update global styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,13 @@
 import { env } from '@/env';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Poppins } from 'next/font/google';
 import Link from 'next/link';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const poppins = Poppins({
   subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins',
 });
 
 export const metadata: Metadata = {
@@ -23,9 +19,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   const isDev = env.NODE_ENV !== 'production';
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${poppins.variable} antialiased`}>
         {isDev && (
           <div className="flex justify-between p-2 print:hidden">
             <Link
