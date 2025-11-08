3296b044da4c5a3f3da4f393481bf16f852ceea3
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2025-11-08 15:11:53 +0100

    feat(layout): integrate ExportButton and Button components for enhanced PDF download and recent years selection

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { ExportButton } from '@/components/client';
+import { Button } from '@/components/server';
 import { env } from '@/env';
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
@@ -15,21 +17,29 @@ export const metadata: Metadata = {
   description: 'Professional Resume of Attila Csanyi',
 };
 
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
+const RootLayout = async ({ children }: { children: React.ReactNode }) => {
   const isDev = env.NODE_ENV !== 'production';
+  const recentYearsOptions = [Infinity, 1, 2, 5, 10];
+
   return (
     <html lang="en">
       <body className={`${poppins.variable} antialiased`}>
         {isDev && (
           <div className="flex justify-between p-2 print:hidden">
-            <Link
-              className="cursor-pointer"
-              href="/api/export"
-            >
-              <button className="rounded-lg bg-linear-to-r from-blue-500 to-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-xs hover:from-blue-600 hover:to-indigo-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-hidden">
-                Download as PDF
-              </button>
-            </Link>
+            <div className="flex gap-2">
+              <ExportButton />
+              {recentYearsOptions.map(year => (
+                <Link
+                  key={year}
+                  className="cursor-pointer"
+                  href={isFinite(year) ? `/?recentYears=${year}` : '/'}
+                >
+                  <Button>
+                    {isFinite(year) ? `Last ${year} years` : 'All years'}
+                  </Button>
+                </Link>
+              ))}
+            </div>
             <div className="flex gap-2">
               <p>Contentful: {env.CONTENTFUL_ENVIRONMENT}</p>
               <p>Node: {env.NODE_ENV}</p>
