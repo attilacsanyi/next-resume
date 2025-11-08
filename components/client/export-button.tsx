d88e2e12634296ad38a618087883938a18e560dd
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2025-11-08 15:11:07 +0100

    feat(components): add ExportButton component for PDF download functionality

---
+++ components/client/export-button.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+import { Button } from '../server';
+
+export const ExportButton = () => {
+  const searchParams = useSearchParams();
+  const queryString = searchParams.toString();
+
+  return (
+    <Link
+      className="cursor-pointer"
+      href={`/api/export${queryString ? `?${queryString}` : ''}`}
+    >
+      <Button>Download as PDF</Button>
+    </Link>
+  );
+};
